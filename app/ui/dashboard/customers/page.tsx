0e9d000e7214de63554d2a3a2061c1eeb8dd62a3
Stepehzzz <zhongliang.stephen@qq.com>
2024-08-19 20:03:07 +0800

    no message

--- app/ui/dashboard/customers/page.tsx
+++ app/ui/dashboard/customers/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/react-in-jsx-scope */
 export default function Page() {
     return <p>Customers Page</p>
 }
--- app/ui/dashboard/layout.tsx
+++ app/ui/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 import SideNav from '@/app/ui/dashboard/sidenav';
+
+export const experimental_ppr = true;
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
